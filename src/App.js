395688950b0a379475e27ac864a50ffc47f1f3e7
Edimar <edimarbmjunior@gmail.com>
2019-11-27 22:41:15 -0300

    salvar projeto

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Routes from './routes';
+
+import './styles.css';
+
+import Header from './components/Header';
+//import Main from './pages/main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* <Main /> */}
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
